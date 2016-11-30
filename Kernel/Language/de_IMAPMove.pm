3567b09027ac11f93bc4f50f78761d3a6e5e2045
Balázs Úr <urbalazs@gmail.com>
2016-11-30 22:38:05 +0100

    Added Hungarian translation

---
+++ Kernel/Language/de_IMAPMove.pm
@@ -0,0 +1,30 @@
+# --
+# Kernel/Language/de_IMAPMove.pm - the German translation of IMAPMove
+# Copyright (C) 2016 Perl-Services, http://www.perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::de_IMAPMove;
+
+use strict;
+use warnings;
+
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation};
+
+    return if ref $Lang ne 'HASH';
+
+    # Kernel/Config/Files/IMAPMove.xml
+    $Lang->{'Defines the folder where mails should be copied to.'} = '';
+
+    return 1;
+}
+
+1;
---
+++ Kernel/Language/hu_IMAPMove.pm
@@ -0,0 +1,32 @@
+# --
+# Kernel/Language/hu_IMAPMove.pm - the Hungarian translation of IMAPMove
+# Copyright (C) 2016 Perl-Services, http://www.perl-services.de
+# Copyright (C) 2016 Balázs Úr, http://www.otrs-megoldasok.hu
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::hu_IMAPMove;
+
+use strict;
+use warnings;
+
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation};
+
+    return if ref $Lang ne 'HASH';
+
+    # Kernel/Config/Files/IMAPMove.xml
+    $Lang->{'Defines the folder where mails should be copied to.'} =
+        'Meghatározza azt a mappát, ahova a leveleket át kell másolni.';
+
+    return 1;
+}
+
+1;
